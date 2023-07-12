d5d510137aea920282d29ed504ad054d1f44e91f
nikorus <nikorus@mail.ru>
2023-07-12 22:03:49 +0300

    Видео 2 Введены 3 части кода:Клиентска,админская и общая Добавлены: функция старта с сообщением консоли о начале работы Декораторы обработки команд Режим_работы,Расположение и сообщения Hello

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
+""" импортируем ос"""
+import os
+
 from aiogram import Bot, types
 from aiogram.dispatcher import Dispatcher
 from aiogram.utils import executor
 
-import os
 from dotenv import load_dotenv
+
 load_dotenv()  # This reads the environment variables inside .env
 
 token = os.getenv('TOKEN')
@@ -13,12 +16,65 @@ bot = Bot(token)
 dp = Dispatcher(bot)
 
 
+async def on_startup(_):
+    """ Функция начального сообщения в консоль о начале работы бота  """
+    print('Бот вышел в онлайн')
+
+
+
+# *************************КЛИЕНСКАЯ ЧАСТЬ **********************
+@dp.message_handler(commands=['start', 'help'])
+async def command_start(message: types.Message):
+    """ Декоратор обработки команд 'start', 'help'  """
+    try:
+        await bot.send_message(message.from_user.id, 'Приятного аппетита')
+        await message.delete()
+    except:
+        await message.reply('Общение с ботом через ЛС, напишите ему первый раз:https://t.me/Nikorus55_bot')
+
+
+@dp.message_handler(commands=["Режим_работы"])
+async def command_start(message: types.Message):
+    await bot.send_message(message.from_user.id,'Каждый день  9-00 до 20-00')
+
+
+
+@dp.message_handler(commands=['Расположение'])
+async def command_start(message: types.Message):
+    """ Декоратор обработки команды  Расположение """
+    await bot.send_message(message.from_user.id, 'Дерибасовская - угол с Мясоедовской')
+
+
+# @dp.message_handler(commands=['Меню'])
+# async def pizza_menu_command(message : types.Message):
+#     for ret in cur.execute('SELECT *FROM menu').fetchall():
+#         await bot.send_photo(message.from_user.id, ret[0],f'{ret[1]}\nЦена {ret[-1]}')
+
+# *************************АДМИНСКАЯ ЧАСТЬ **********************
+
+# *************************ОБЩАЯ ЧАСТЬ **********************
+
+
+# Прописываем декоратор , который будет обработываьть сообщения пришедшие в месседжер
 @dp.message_handler()
 # декорируемая ф-я
 async def echo_send(message: types.Message):  # имя функции любое
-    await message.answer(message.text)  # 1-й способ
+    """
+    Декоратор обработки сообщения пользователя Hello
+    :param message:
+    :return:
+    """
+    if message.text == 'Hello':
+        await message.answer('Hi, my darling !')
+
+    # await message.answer(message.text)  # 1-й способ
     # await message.reply(message.text)  # 2-й способ тоже, о с повторением исходного сообщения
-    # await bot.send_message(message.from_user.id, message.text)  #3-й способ - только если пользователь уже обращался к боту
+    # await bot.send_message(message.from_user.id, message.text)
+    # #3-й способ - только если пользователь уже обращался к боту
+
+
+# здесь получается айди пользователя и посылается ему сообщение
+
 
 # запуск бота с параметром пропускать обновления - все сообщения которые приходят боту в офлайн
-executor.start_polling(dp, skip_updates=True)
+executor.start_polling(dp, skip_updates=True, on_startup=on_startup)
