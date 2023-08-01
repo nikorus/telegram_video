9fb86d582ceac191cf5b427400c235398fde79c4
nikorus <nikorus@mail.ru>
2023-08-01 13:50:33 +0300

    Видео 10 - 1: Парсинг сайта youtube на python - telegram бот в inline режиме парсит поисковую выдачу 	Парсим поисковую выдачу на python с requests, beautifulsoup, api или воспользуемся готовым парсером? В видео посмотрим как создать inline бот парсер на python с aiogram и приделать к нему онлайн парсер. В некоторых случаях парсинг сайтов на питон очень легок, особенно когда есть готовое решение) 	Создаем отлеьную папку в Проекте – parsing.  И в ней 2 модуляЖ вспомогательный -Dubl  и основной – ytparsingBot/ Во вспомогательном модуле испортируем все нужные библиотеки, установим виртуальное окружение, инициальзируем обекты бота  и его данных,  делаем поллинг Проверяем что он работает и Закоментируем его Импортируем библиотеки запросов и парсинга – requests и bt4, Xmls -парсер ( как еще один вариант парсинга) (В принципе существует огромноле число других библиотек для парсинга и даже api для парсируемого сайта.) Все идентификаторы видео у ютьба спрятаны в одном тэге, потому чтобы добраться до конкретного нужно спарсить Импортируем необходимые библиотеки Записываем новую функцию 	В этой функции сначала посылается запрос на получения данных по конкретному юрл адресу. Для примера мы его пропишим как поиск python Далее создаем объект из полученного результата В этом объекте ищим все объекты с именем 'script' И теперь распечатаем результат print(search)  И запускаем код для проверки Ничего конкретно не нашлось - поэтому оставляем этот подход и пробуем использовать html.parser Этот вариант показан в Видео10-2

---
+++ parsing/dubl.py
@@ -0,0 +1,42 @@
+
+
+
+
+
+
+# import os
+# from aiogram import Bot, types
+# from aiogram.dispatcher import Dispatcher
+# from aiogram.utils import executor
+# from aiogram.types import InputTextMessageContent, InlineQueryResultArticle
+# from dotenv import load_dotenv
+#
+# load_dotenv()  # This reads the environment variables inside .env
+# token = os.getenv('TOKEN_INLINE')
+# # инициализируем бот
+# bot = Bot(token)
+# # инициализируем диспетчер, передавая в него экземпляр бота
+# # dp = Dispatcher(bot)
+# #  добавляем параметр бота, где будет хранится информация
+# dp = Dispatcher(bot=bot)
+#
+# executor.start_polling(dp, skip_updates=True)
+
+import requests
+from bs4 import BeautifulSoup
+import re
+import lxml
+
+def searcher():
+    response = requests.get('https://www.youtube.com/results?search_query=python')
+    soup = BeautifulSoup(response.content, 'lxml')
+
+    search = soup.find_all('script')
+    # search = soup.find_all('script')[32]  # это номер в списке для примера
+    # key = '"videoID":"'
+    # data = re.findall(key+r"([^*]{11})", str(search)) #  [^*], обозначает любые символы кроме *!
+    # print(data)
+    print(search)
+
+
+searcher()
